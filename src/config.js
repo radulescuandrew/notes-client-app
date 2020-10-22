5f260daadc86a93ba0826bced88db6f09da0836a
Andrew Radulescu <radulescuandrew@gmail.com>
2020-10-22 11:16:16 +0300

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-central-1",
+        BUCKET: "serverless-stack-notes-app-uploads-ndru"
+    },
+    apiGateway: {
+        REGION: "eu-central-1",
+        URL: "https://9ddr5ln8kf.execute-api.eu-central-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "eu-central-1",
+        USER_POOL_ID: "eu-central-1_Jzz33Xd82",
+        APP_CLIENT_ID: "it8ojvit49tqbumif3bmsnru1",
+        IDENTITY_POOL_ID: "eu-central-1:de8a592a-469d-4056-9a72-f066c3793fcd"
+    }
+};
